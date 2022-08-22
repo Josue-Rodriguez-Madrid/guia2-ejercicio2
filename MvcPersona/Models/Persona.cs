54849eaff684eaa5880b15fd1e9cb4577ccc6ea3
Josue <68652808+Josue-Rodriguez-Madrid@users.noreply.github.com>
2022-08-22 12:21:42 -0600

    Add files via upload

---
+++ MvcPersona/Models/Persona.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcPersona.Models
+{
+    public class Persona
+    {
+        public int ID { get; set; }
+        public string Dui { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        [Display(Name = "Fecha de Nacimiento")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode
+= true)]
+        public DateTime FechaNacimiento { get; set; }
+        public string Direccion { get; set; }
+        public string Correo { get; set; }
+        
+    }
+    public class PersonasDBContext : DbContext
+    {
+        public DbSet<Persona> Personas { get; set; }
+    }
+}
