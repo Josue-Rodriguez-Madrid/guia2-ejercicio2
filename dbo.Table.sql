9c05699e0813564351b12a25b99a4fd541e92f5d
Josue <68652808+Josue-Rodriguez-Madrid@users.noreply.github.com>
2022-08-22 11:52:49 -0600

    Add files via upload

---
+++ dbo.Table.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY  IDENTITY (1, 1), 
+    [Dui] VARCHAR(50) NOT NULL, 
+    [Nombre] VARCHAR(50) NOT NULL, 
+    [Apellido] VARCHAR(50) NOT NULL, 
+    [FechaNacimiento] DATETIME NOT NULL, 
+    [Direccion] VARCHAR(50) NOT NULL, 
+    [Correo] VARCHAR(50) NOT NULL
+)
